ef4a2594c157e77c8e6c3a3f5e89f898402d37ea
Adrian Head <adrian.head14@gmail.com>
2018-10-24 01:19:48 +0000

    added db connection

---
+++ includes/database.php
@@ -0,0 +1,12 @@
+<?php
+$host = getenv('HOST');
+$username = getenv('USERNAME');
+$password = getenv('PASSWORD');
+$database = getenv('DATABASE');
+
+$connection = mysqli_connect($host,$username,$password,$database);
+if( $connection == false){
+    echo "database connection error";
+}
+
+?>
